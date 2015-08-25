2121dffea9bf30e13a720e349483be32e253188f
Ned Lucks <ned@bluetent.com>
2015-08-24 22:40:17 -0600

    Initial commit.

---
+++ js/jquery.inntopia.js
@@ -0,0 +1,30 @@
+(function($) {
+  Drupal.behaviors.inntopiaWidget = {
+    attach: function(context, settings) {
+      $('.inntopia-reponsive-commerce-form', context).once('inntopiaWidget', function () {
+        var widget = this;
+        $('.inntopia-date[name=arrivaldate], .inntopia-date[name=departuredate], .inntopia-date[name=startdate]', this).datepicker({
+          dateFormat: 'mm/dd/yy',
+          numberOfMonths: 1,
+          stepMonths: 1,
+          showOn: 'both',
+          minDate: 1,
+          maxDate: 365,
+          changeMonth: true,
+          changeYear: true
+        });
+        $('.inntopia-date[name=departuredate]', widget).datepicker('option', 'beforeShow', function(input, inst) {
+            if($('.inntopia-date[name=arrivaldate]', widget).val()) {
+              return {
+                minDate: $('.inntopia-date[name=arrivaldate]', widget).datepicker("getDate"),
+                disabled: false
+              };
+            } else {
+              return {disabled: true};
+            }
+          }
+        );
+      });
+    }
+  }
+})(jQuery);
---
+++ theme/inntopia-responsive-commerce-widget.tpl.php
@@ -0,0 +1,19 @@
+<script>
+  var inntopiaVariables = {
+    salesId: <?php print $salesid; ?>, //replace '000000' with your salesId
+    widgetType: <?php print $supplierid ? '3' : '1'; ?>, //Select one of the following: 1=lodging search,  2=activity search, 3=hotel/location lodging search
+    productSuperCategoryId: null, //required for widgetType: 2. Refer to http://www.inntopia.com/inntopia/templates/reports/productcategories_RAW.xml
+    widgetLayout: 'horizontal', //Select one of the following: 'horizontal' or 'vertical'
+    supplierId: <?php print $supplierid ?: 'null'; ?>, //required for widgetType: 3
+    productCategoryId: 117, //optional for widgetType: 2. to http://www.inntopia.com/inntopia/templates/reports/productcategories_RAW.xml
+    startDateLabel: 'Starting', //for activity search labels
+    arrivalDateLabel: 'Arrival', //for lodging search labels
+    departureDateLabel: 'Departure', //for lodging search labels
+    adultLabel: 'Adults', //for lodging search labels
+    childLabel: 'Children', //for lodging search labels
+    buttonText: 'Check Availability', //button text for all widget types
+    domain: 'www.inntopia.travel', //update with your custom domain, if available
+    language: 'en-US' //only en-US currently supported for this widget
+  };
+</script>
+<div id="inntopiaWidget"></div>
